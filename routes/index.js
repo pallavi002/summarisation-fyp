9696ca275cbb4eff4fe0aed288f2464d6df2e794
pallavi002 <pallavilodhi08@gmail.com>
2021-10-29 19:27:07 +0530

    view setup

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/summarise', function(req, res) {
+  res.send('Hello we summarise here')
+})
+
 module.exports = router;
