a2545399c4c3b602e7a0a53bbe594d13cd673eb7
pallavi002 <pallavilodhi08@gmail.com>
2021-12-06 13:46:41 +0530

    saved audio text to file

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/summarise', function(req, res) {
+router.get('/summarise', function (req, res) {
   res.send('Hello we summarise here')
+})
+
+router.post('/summarise', function (req, res) {
+  console.log(req.body.data)
+
+  const content = req.body.data;
+
+  try {
+    fs.writeFileSync('./Output.txt', content)
+    //file written successfully
+  } catch (err) {
+    console.error(err)
+  }
 })
 
 module.exports = router;
