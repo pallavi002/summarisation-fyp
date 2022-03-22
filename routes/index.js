28cb22844f4b2cf8ed8d330540d6cf14a264a1fa
pallavi002 <pallavilodhi08@gmail.com>
2022-03-22 15:59:35 +0530

    this is the backup if og doesn't work

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
+const res = require('express/lib/response');
 var router = express.Router();
 var formidable = require('formidable');
 const fs = require('fs', 'fs-extra');
 const { Translate } = require('@google-cloud/translate').v2;
+SummarizerManager = require("node-summarizer").SummarizerManager;
 require('dotenv').config();
 
 
@@ -32,17 +34,17 @@ router.post('/summarise', function (req, res) {
 })
 
 router.post('/fileupload', function (req, res) {
-   var form = new formidable.IncomingForm();
-     //Formidable uploads to operating systems tmp dir by default
-     form.uploadDir = "./";       //set upload directory
-     form.keepExtensions = true;     //keep file extension
- 
-     form.parse(req, function(err, fields, files) {
-        fs.rename('./invalid-name', './Output.txt', () => {
-          console.log("\nFile Renamed!\n");
-        });
-          res.redirect('back')
-     });
+  var form = new formidable.IncomingForm();
+  //Formidable uploads to operating systems tmp dir by default
+  form.uploadDir = "./";       //set upload directory
+  form.keepExtensions = true;     //keep file extension
+
+  form.parse(req, function (err, fields, files) {
+    fs.rename('./invalid-name', './Output.txt', () => {
+      console.log("\nFile Renamed!\n");
+    });
+    res.redirect('back')
+  });
 })
 
 router.post('/summarisehindi', function (req, res) {
@@ -83,6 +85,44 @@ router.post('/summarisehindi', function (req, res) {
     .catch((err) => {
       console.log(err);
     });
+})
+
+router.get('/getsummary', function (req, res) {
+  const fs = require("fs");
+  var summarydata, reduction_accuracy;
+  // __dirname means relative to script. Use "./data.txt" if you want it relative to execution path.
+  fs.readFile("./Output.txt", (error, data) => {
+    if (error) {
+      throw error;
+    }
+    var filedata = data.toString();
+    // console.log(filedata)
+    Summarizer = new SummarizerManager(filedata, 5);
+    summary = Summarizer.getSummaryByRank().then((summary_object) => {
+      // console.log(summary_object.summary)
+      summarydata = summary_object.summary
+      // res.render('summarypage')
+    })
+    let reduction_percentage = Summarizer.getRankReductionAsDec().then((reduction_obj)=>{
+      reduction_accuracy = Math.round(reduction_obj.dec_reduction *100, 2);
+  })
+
+    // setTimeout(() => {console.log("consss...... ", summarydata)}, 5000);
+    setTimeout(async () => {
+
+      try {
+        fs.writeFileSync('./summaryoutput.txt', summarydata);
+        // alert("Audio Saved Successfully!")
+        //file written successfully
+        res.render('summarypage', { summary: summarydata, accuracy: reduction_accuracy })
+      } catch (err) {
+        console.error(err);
+        // res.redirect('back')
+      }
+      // console.log("summary..",Summarizer)
+    }, 3000);
+
+  })
 })
 
 module.exports = router;
